40a81a22dad446a7679c8d978898ee82dacd9d57
Léo Paillard <leo.paillard@gmail.com>
2024-12-09 23:15:39 +0100

    Day 4 ok

--- src/day4.rs
+++ src/day4.rs
@@ -51,88 +51,58 @@ impl Grid {
         self.get_index(pos).and_then(|index| self.cells.get(index))
     }
 
-    fn get_up_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x, y - 1),
-            Position::new(x, y - 2),
-            Position::new(x, y - 3),
-        ]
-    }
-
-    fn get_up_right_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x + 1, y - 1),
-            Position::new(x + 2, y - 2),
-            Position::new(x + 3, y - 3),
-        ]
-    }
-
-    fn get_right_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x + 1, y),
-            Position::new(x + 2, y),
-            Position::new(x + 3, y),
-        ]
-    }
-
-    fn get_lo_right_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x + 1, y + 1),
-            Position::new(x + 2, y + 2),
-            Position::new(x + 3, y + 3),
-        ]
-    }
-
-    fn get_lo_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x, y + 1),
-            Position::new(x, y + 2),
-            Position::new(x, y + 3),
-        ]
-    }
-
-    fn get_lo_left_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x - 1, y + 1),
-            Position::new(x - 2, y + 2),
-            Position::new(x - 3, y + 3),
-        ]
-    }
-
-    fn get_left_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x - 1, y),
-            Position::new(x - 2, y),
-            Position::new(x - 3, y),
-        ]
-    }
-
-    fn get_up_left_pos_from(x: isize, y: isize) -> [Position; 4] {
-        [
-            Position::new(x, y),
-            Position::new(x - 1, y - 1),
-            Position::new(x - 2, y - 2),
-            Position::new(x - 3, y - 3),
-        ]
-    }
+    // PART 1
 
     fn count_matches_from(&self, x: isize, y: isize) -> usize {
         let positions: Vec<[Position; 4]> = vec![
-            Self::get_up_pos_from(x, y),
-            Self::get_up_right_pos_from(x, y),
-            Self::get_right_pos_from(x, y),
-            Self::get_lo_right_pos_from(x, y),
-            Self::get_lo_pos_from(x, y),
-            Self::get_lo_left_pos_from(x, y),
-            Self::get_left_pos_from(x, y),
-            Self::get_up_left_pos_from(x, y),
+            [
+                Position::new(x, y),
+                Position::new(x, y - 1),
+                Position::new(x, y - 2),
+                Position::new(x, y - 3),
+            ], // UP
+            [
+                Position::new(x, y),
+                Position::new(x + 1, y - 1),
+                Position::new(x + 2, y - 2),
+                Position::new(x + 3, y - 3),
+            ], // UP RIGHT
+            [
+                Position::new(x, y),
+                Position::new(x + 1, y),
+                Position::new(x + 2, y),
+                Position::new(x + 3, y),
+            ], // RIGHT
+            [
+                Position::new(x, y),
+                Position::new(x + 1, y + 1),
+                Position::new(x + 2, y + 2),
+                Position::new(x + 3, y + 3),
+            ], // DOWN RIGHT
+            [
+                Position::new(x, y),
+                Position::new(x, y + 1),
+                Position::new(x, y + 2),
+                Position::new(x, y + 3),
+            ], // DOWN
+            [
+                Position::new(x, y),
+                Position::new(x - 1, y + 1),
+                Position::new(x - 2, y + 2),
+                Position::new(x - 3, y + 3),
+            ], // DOWN LEFT
+            [
+                Position::new(x, y),
+                Position::new(x - 1, y),
+                Position::new(x - 2, y),
+                Position::new(x - 3, y),
+            ], // LEFT
+            [
+                Position::new(x, y),
+                Position::new(x - 1, y - 1),
+                Position::new(x - 2, y - 2),
+                Position::new(x - 3, y - 3),
+            ], // UP LEFT
         ];
 
         let mut matches = 0;
@@ -155,9 +125,59 @@ impl Grid {
 
         matches
     }
+
+    // PART 2
+
+    fn matches_from(&self, x: isize, y: isize) -> bool {
+        let Some(up_r) = self.get(Position::new(x + 1, y - 1)) else {
+            return false;
+        };
+
+        let Some(do_r) = self.get(Position::new(x + 1, y + 1)) else {
+            return false;
+        };
+
+        let Some(do_l) = self.get(Position::new(x - 1, y + 1)) else {
+            return false;
+        };
+
+        let Some(up_l) = self.get(Position::new(x - 1, y - 1)) else {
+            return false;
+        };
+
+        Self::chars_matches(*up_r, *do_l) && Self::chars_matches(*do_r, *up_l)
+    }
+
+    fn chars_matches(l: char, r: char) -> bool {
+        (l == 'M' && r == 'S') || (l == 'S' && r == 'M')
+    }
 }
 
 pub(crate) fn day_4_1() {
+    let lines = include_str!("../data/day4.txt").lines();
+
+    let width = lines.clone().next().unwrap().len() as isize;
+    let height = lines.clone().count() as isize;
+    let cells = lines.collect::<Vec<&str>>().join("").chars().collect();
+
+    let grid = Grid::new(width, height, cells);
+    let mut matches: usize = 0;
+
+    for y in 0..grid.height() {
+        for x in 0..grid.width() {
+            let pos = Position::new(x, y);
+            if let Some(c) = grid.get(pos) {
+                if *c == 'X' {
+                    matches += grid.count_matches_from(x, y);
+                }
+            }
+        }
+    }
+
+    println!("Number of matches = {matches}");
+}
+
+pub(crate) fn day_4_2() {
     let lines = include_str!("../data/day4.txt").lines();
     let width = lines.clone().next().unwrap().len() as isize;
     let height = lines.clone().count() as isize;
@@ -166,20 +186,18 @@ pub(crate) fn day_4_1() {
 
     let cells = lines.collect::<Vec<&str>>().join("").chars().collect();
     let grid = Grid::new(width, height, cells);
-    let mut matches_count: usize = 0;
+    let mut matches: usize = 0;
 
     for y in 0..grid.height() {
         for x in 0..grid.width() {
             let pos = Position::new(x, y);
             if let Some(c) = grid.get(pos) {
-                if *c == 'X' {
-                    matches_count += grid.count_matches_from(x, y);
+                if *c == 'A' && grid.matches_from(x, y) {
+                    matches += 1;
                 }
             }
         }
     }
 
-    println!("Matches = {matches_count}");
+    println!("Number of matches = {matches}");
 }
-
-pub(crate) fn day_4_2() {}
