a34932670b8fad5970b18ca64fa1d8ee0533bfdb
Léo Paillard <leo.paillard@gmail.com>
2024-12-10 15:15:16 +0100

    day 5 1

---
+++ src/day5.rs
@@ -0,0 +1,90 @@
+#[derive(Debug)]
+struct Rule {
+    left: i32,
+    right: i32,
+}
+
+impl Rule {
+    fn new(left: i32, right: i32) -> Self {
+        Rule { left, right }
+    }
+
+    fn left(&self) -> i32 {
+        self.left
+    }
+
+    fn right(&self) -> i32 {
+        self.right
+    }
+
+    fn apply_to(&self, update: &Update) -> bool {
+        update.pages.contains(&self.left) && update.pages.contains(&self.right)
+    }
+}
+
+#[derive(Debug)]
+struct Update {
+    pages: Vec<i32>,
+}
+
+impl Update {
+    fn new(pages: Vec<i32>) -> Self {
+        Update { pages }
+    }
+
+    fn respect(&self, rule: &Rule) -> bool {
+        self.pages
+            .iter()
+            .position(|&p| p == rule.left())
+            .expect("page not found")
+            < self
+                .pages
+                .iter()
+                .position(|&p| p == rule.right())
+                .expect("page not found")
+    }
+
+    fn mid(&self) -> Option<&i32> {
+        self.pages.get(self.pages.len() / 2)
+    }
+}
+
+pub(crate) fn day_5_1() {
+    let rules: Vec<Rule> = include_str!("../data/day5.txt")
+        .lines()
+        .take_while(|line| *line != "//")
+        .collect::<Vec<&str>>()
+        .into_iter()
+        .map(|s: &str| {
+            let split: Vec<&str> = s.split('|').collect();
+            Rule::new(
+                split.first().unwrap().parse::<i32>().unwrap(),
+                split.last().unwrap().parse::<i32>().unwrap(),
+            )
+        })
+        .collect();
+
+    let updates: Vec<Update> = include_str!("../data/day5.txt")
+        .lines()
+        .skip_while(|line| *line != "//")
+        .skip(1)
+        .collect::<Vec<&str>>()
+        .into_iter()
+        .map(|s: &str| Update::new(s.split(',').map(|s| s.parse::<i32>().unwrap()).collect()))
+        .collect();
+
+    let total: i32 = updates
+        .iter()
+        .filter(|update| {
+            let applied_rules: Vec<&Rule> =
+                rules.iter().filter(|rule| rule.apply_to(update)).collect();
+
+            applied_rules.iter().all(|rule| update.respect(rule))
+        })
+        .map(|update| *update.mid().unwrap())
+        .sum();
+
+    println!("Total : {total:#?}");
+}
+
+pub(crate) fn day_5_2() {}
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 use day1::{day_1_1, day_1_2};
 use day2::{day_2_1, day_2_2};
 use day3::{day_3_1, day_3_2};
 use day4::{day_4_1, day_4_2};
+use day5::{day_5_1, day_5_2};
 
 fn main() {
     // day_1_1();
@@ -19,5 +21,6 @@ fn main() {
     // day_3_1();
     // day_3_2();
     // day_4_1();
-    day_4_2();
+    // day_4_2();
+    day_5_1();
 }
